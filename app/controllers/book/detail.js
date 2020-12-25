97c6bfaad1b8b048cc3b3a8d8286515199f4a4d9
Andrey Vershinin <adw006@ya.ru>
2020-12-25 13:24:19 +0300

    feat: add time formatting with moment

--- app/controllers/book/detail.js
+++ app/controllers/book/detail.js
@@ -2,10 +2,6 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class BookDetailController extends Controller {
-  get displayDate() {
-    return this.model.publishDate?.slice(0, 10) || '';
-  }
-
   @action
   async deleteBook() {
     await this.model.destroyRecord();
