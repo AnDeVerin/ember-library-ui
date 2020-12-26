4d677e6fbc08add31e05ec2ff028a963e9110185
Andrey Vershinin <adw006@ya.ru>
2020-12-26 14:33:56 +0300

    feat: transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,4 +1,26 @@
-const duration = 200;
+const duration = 400;
+
+const slidingCardIn = [
+  {
+    pickNew: '.slide-out-bg',
+    use: ['fade', { duration: 200 }],
+  },
+  {
+    pickNew: '.slide-out-card',
+    use: ['wait', 200, { then: 'toLeft' }, { duration }],
+  },
+];
+
+const slidingCardOut = [
+  {
+    pickOld: '.slide-out-bg',
+    use: ['wait', duration, { then: 'fade' }, { duration: 200 }],
+  },
+  {
+    pickOld: '.slide-out-card',
+    use: ['toRight', { duration }],
+  },
+];
 
 export default function () {
   this.transition(this.hasClass('modal-if'), this.use('fade', { duration }));
@@ -13,5 +35,17 @@ export default function () {
     this.fromRoute('author.detail.index'),
     this.use('fade', { duration }),
     this.reverse('fade', { duration })
+  );
+
+  this.transition(
+    this.fromRoute('author.index'),
+    this.use('explode', ...slidingCardIn),
+    this.reverse('explode', ...slidingCardOut)
+  );
+
+  this.transition(
+    this.fromRoute('book.index'),
+    this.use('explode', ...slidingCardIn),
+    this.reverse('explode', ...slidingCardOut)
   );
 }
